bb058517cbb3d8e6ea3a3f79394264f9b81cf134
Himanshu <153494576+himanshu0470@users.noreply.github.com>
2024-09-23 01:48:27 +0530

    added socket changes

--- src/components/WebRtc/src/socket.js
+++ src/components/WebRtc/src/socket.js
@@ -1,30 +1,30 @@
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
 // const ENDPOINT = 'http://localhost:3001/';
-const ENDPOINT = 'https://serverzoom-mpbv.onrender.com/'; // Uncomment this line for production
+const ENDPOINT = "https://wevrtcbe-1.onrender.com/"; // Uncomment this line for production
 
 const socket = io(ENDPOINT, {
   autoConnect: true,
   forceNew: true,
-  transports: ['websocket', 'polling'],
+  transports: ["websocket", "polling"],
   reconnection: true,
   reconnectionAttempts: Infinity,
   reconnectionDelay: 1000,
 });
 
-socket.on('connect', () => {
-  console.log('Socket connected with ID:', socket.id);
+socket.on("connect", () => {
+  console.log("Socket connected with ID:", socket.id);
 });
 
-socket.on('connect_error', (error) => {
-  console.error('Socket connection error:', error);
+socket.on("connect_error", (error) => {
+  console.error("Socket connection error:", error);
   // Attempt to reconnect
   socket.connect();
 });
 
-socket.on('disconnect', (reason) => {
-  console.log('Socket disconnected:', reason);
-  if (reason === 'io server disconnect') {
+socket.on("disconnect", (reason) => {
+  console.log("Socket disconnected:", reason);
+  if (reason === "io server disconnect") {
     // the disconnection was initiated by the server, you need to reconnect manually
     socket.connect();
   }
@@ -32,18 +32,24 @@ socket.on('disconnect', (reason) => {
 
 // Debug: Log all emitted events
 const originalEmit = socket.emit;
-socket.emit = function() {
-  console.log('Socket emitting:', arguments[0], arguments[1], 'Current socket ID:', socket.id);
+socket.emit = function () {
+  console.log(
+    "Socket emitting:",
+    arguments[0],
+    arguments[1],
+    "Current socket ID:",
+    socket.id
+  );
   return originalEmit.apply(socket, arguments);
 };
 
 // Function to check connection status
 const checkConnection = () => {
   if (!socket.connected) {
-    console.log('Socket not connected, attempting to connect...');
+    console.log("Socket not connected, attempting to connect...");
     socket.connect();
   } else {
-    console.log('Socket is connected. ID:', socket.id);
+    console.log("Socket is connected. ID:", socket.id);
   }
 };
 
